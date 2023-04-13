cee257071061bb2b109fa32ede80b35ef842be07
Mac <mac.rusek@pm.me>
2023-04-13 23:47:00 +0100

    user service update - test red

--- src/services/usertService.spec.ts
+++ src/services/usertService.spec.ts
@@ -1,4 +1,4 @@
-import { User } from "../models/types";
+import { User, UserUpdate } from "../models/types";
 import * as userService from "./userService";
 import * as userRepo from "../repositories/userRepository";
 
@@ -19,6 +19,37 @@ describe("User Service", () => {
     createMock.mockResolvedValue({ ...userNoId, id: "some_id" });
 
     const result = await userService.add(userNoId);
+
+    expect(result).toHaveProperty("id");
+  });
+
+  it("Updates user", async () => {
+    const userToUpdate: UserUpdate = {
+      id: "some_id",
+      address: {
+        street1: "Flat 1, New Building",
+        street2: "10 New Street",
+        city: "Oxford",
+        postcode: "OX10 3OX",
+      },
+    };
+
+    const userWithNewAddress = {
+      id: "some_id",
+      name: "john doe",
+      dob: "01-01-1900",
+      phone: "+447000000000",
+      address: {
+        street1: "Flat 1, New Building",
+        street2: "10 New Street",
+        city: "Oxford",
+        postcode: "OX10 3OX",
+      },
+    };
+    const createMock = jest.spyOn(userRepo, "update");
+    createMock.mockResolvedValue(userWithNewAddress);
+
+    const result = await userService.update(userToUpdate);
 
     expect(result).toHaveProperty("id");
   });
