73e2db7622774e05f5f2c9a6e925f32c59f64d8c
Mac <mac.rusek@pm.me>
2023-04-14 10:06:14 +0100

    user handler update - tests red

--- src/handlers/userHandler.spec.ts
+++ src/handlers/userHandler.spec.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { addUser } from "./userHandler";
-import { User } from "../models/types";
+import { User, UserUpdate } from "../models/types";
 import * as userService from "../services/userService";
 
 describe("App Handlers", () => {
@@ -77,6 +77,84 @@ describe("App Handlers", () => {
       });
 
       const result = await addUser(payload);
+
+      expect(result).toStrictEqual({
+        statusCode: 400,
+        body: `Bad Request`,
+      });
+    });
+  });
+
+  describe("update user", () => {
+    const userToUpdate: UserUpdate = {
+      id: "some_id",
+      address: {
+        street1: "Flat 1, New Building",
+        street2: "10 New Street",
+        city: "Oxford",
+        postcode: "OX10 3OX",
+      },
+    };
+    it("Returns stringified results on update user", async () => {
+      const payload = {
+        body: JSON.stringify(userToUpdate),
+        httpMethod: "PUT",
+      } as any as APIGatewayProxyEvent;
+
+      const createMock = jest.spyOn(userService, "update");
+      createMock.mockResolvedValue({ ...user, ...userToUpdate });
+
+      const result = await updateUser(payload);
+
+      expect(result).toStrictEqual({
+        statusCode: 200,
+        body: JSON.stringify({
+          ...user,
+          ...userToUpdate,
+        }),
+      });
+    });
+
+    it("Throws when wrong httpMethod is used", async () => {
+      const payload = {
+        body: JSON.stringify(user),
+        httpMethod: "GET",
+      } as any as APIGatewayProxyEvent;
+
+      const result = await updateUser(payload);
+
+      expect(result).toStrictEqual({
+        statusCode: 500,
+        body: `Only accepts POST method, you tried: GET method.`,
+      });
+    });
+
+    it("Throws when empty no body is provided", async () => {
+      const payload = {
+        httpMethod: "PUT",
+      } as any as APIGatewayProxyEvent;
+
+      const result = await updateUser(payload);
+
+      expect(result).toStrictEqual({
+        statusCode: 500,
+        body: `Empty payment body`,
+      });
+    });
+
+    it("Throws when service errors out", async () => {
+      const payload = {
+        body: JSON.stringify(user),
+        httpMethod: "PUT",
+      } as any as APIGatewayProxyEvent;
+
+      const createMock = jest.spyOn(userService, "update");
+      createMock.mockRejectedValue({
+        statusCode: 400,
+        message: "Bad Request",
+      });
+
+      const result = await updateUser(payload);
 
       expect(result).toStrictEqual({
         statusCode: 400,
