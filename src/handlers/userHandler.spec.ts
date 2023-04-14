7d2d7051a88d85f8a9b72fbf088c3c248b89265e
Mac <mac.rusek@pm.me>
2023-04-14 09:44:30 +0100

    user handler add - test red

---
+++ src/handlers/userHandler.spec.ts
@@ -0,0 +1,37 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { addUser } from "./userHandler";
+import { User } from "../models/types";
+
+describe("App Handlers", () => {
+  describe("add user", () => {
+    it("Returns stringified results on add user", async () => {
+      const id = "some_id";
+      const user: User = {
+        name: "john doe",
+        dob: "01-01-1900",
+        phone: "+447000000000",
+        address: {
+          street1: "Flat 1, Some Building",
+          street2: "10 Nice Street",
+          city: "London",
+          postcode: "LL10 3LL",
+        },
+      };
+
+      const payload = {
+        body: JSON.stringify(user),
+        httpMethod: "POST",
+      } as any as APIGatewayProxyEvent;
+
+      const result = await addUser(payload);
+
+      expect(result).toStrictEqual({
+        statusCode: 200,
+        body: JSON.stringify({
+          ...user,
+          id,
+        }),
+      });
+    });
+  });
+});
